c156a0d61ef373ca076365394817aa959c8028c2
DanhBui <bxdanh1998@gmail.com>
2023-04-14 11:05:12 +0700

    #2. Cấu hình react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import { routes } from './routes';
+
 import './App.scss';
+import DefaultLayout from './layouts/DefaultLayout';
 
 function App() {
-    return <div className="App">DBShop ReactJS với Vite</div>;
+    return (
+        <div className="App">
+            <Routes>
+                {routes.map((route, index) => {
+                    let Page = route.element;
+                    return (
+                        <Route
+                            key={index}
+                            element={
+                                <DefaultLayout>
+                                    <Page />
+                                </DefaultLayout>
+                            }
+                            path={route.path}
+                        />
+                    );
+                })}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+import { PATH } from '../configs/path';
+import Contact from '../pages/contact';
+import Home from '../pages/home';
+
+export const routes = [
+    { element: Home, path: PATH.home },
+    { element: Contact, path: PATH.contact },
+];
